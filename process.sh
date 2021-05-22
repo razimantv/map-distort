072d7c4db95acd0f36cba6977fbeea59ce26021a
Raziman T V <razimantv@gmail.com>
2021-05-22 14:45:08 +0200

    Move map to data

--- process.sh
+++ process.sh
@@ -5,13 +5,13 @@ mkdir -p images data/processed
 cd data
 echo "Generating map data"
 echo "" original.dat
-echo ../states_boundary.dat population.dat 1 | ../../weighted_distort \
+echo states_boundary.map population.dat 1 | ../weighted_distort \
   > processed/original.dat 2>processed/log_original.dat
 
 for i in *.dat
 do
   echo "" $i
-  echo ../states_boundary.dat $i 10000 | ../../weighted_distort \
+  echo states_boundary.map $i 10000 | ../weighted_distort \
     > processed/$i 2>processed/log_$i
 done
 echo done
